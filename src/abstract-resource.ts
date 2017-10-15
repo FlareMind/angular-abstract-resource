60e2ea2764d59ef4dd055955e9bda0e7e71437a3
afsa <mattulainen@gmail.com>
2017-10-15 17:22:20 +0200

    Resource update WIP

--- src/abstract-resource.ts
+++ src/abstract-resource.ts
@@ -1,9 +1,36 @@
+import 'core-js/es6/object'
 import {IConfig} from "./interfaces/config";
 
-export class AbstractResourceFactory {
+const REQUIRED_VALUES : string[] = ['url'];
 
-    static factory(config : IConfig) {
+const DEFAULT_VALUES : IConfig = {
+    url: '',
+    trailingUrlSlash: true,
+    extension: () => {},
+    single: false,
+    update: 0
+};
 
-    }
+export let AbstractResource = (config : IConfig) => ($resource : ng.resource.IResourceService, $injector : ng.auto.IInjectorService, $q : ng.IQService, $timeout : ng.ITimeoutService) => {
+    this.$inject = ['$resource', '$injector', '$q', '$timeout'];
 
-}
+    /*
+     * Check that all the required properties are set
+     */
+    (() => {
+        let keys = Object.keys(config);
+        REQUIRED_VALUES.forEach(item => {
+            if (keys.indexOf(item) === -1) {
+                throw new Error('AbstractResource config requires the property `' + item + '`!');
+            }
+        });
+    })();
+
+    config = Object.assign({}, DEFAULT_VALUES, config);
+
+    let resource : any = config.single ? {} : [];
+
+    let updateResource = () => $q((resolve : Function, reject : Function) => {
+
+    });
+};
--- src/interfaces/config.ts
+++ src/interfaces/config.ts
@@ -1,6 +1,7 @@
 
 export interface IConfig {
     url : string;
+    trailingUrlSlash?: boolean;
     extension?: Function;
     single?: boolean;
     update?: number;
